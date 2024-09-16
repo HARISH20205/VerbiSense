e1e29462f6c52161705630bd96edadb2d6be4ed7
Daranivelan <daranivelan10@gmail.com>
2024-09-16 13:05:18 +0530

    Welcome Page, Login Page, Signup Page

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import logo from "./assets/iconoir--page.svg"
 
 function Signup() {
   return (
-    <div>Signup</div>
+    <div className="flex flex-col justify-center items-center h-screen gap-10">
+      <div className="flex flex-col items-center gap-2">
+        <img src={logo} className='w-8'></img>
+        <p className='text-gray-900 font-semibold text-2xl'>Create your account</p>
+      </div>
+      <div className="flex flex-col gap-7">
+        <input className='border-2 border-gray-300 rounded-md w-96 py-1 pl-3 focus:outline-none focus:bg-blue-100 active:bg-blue-100' type='text' placeholder='Full name'></input>
+        <input className='border-2 border-gray-300 rounded-md w-96 py-1 pl-3 focus:outline-none focus:bg-blue-100' type='email' placeholder='Email address'></input>
+        <input className='border-2 border-gray-300 rounded-md w-96 py-1 pl-3 focus:outline-none' type='password' placeholder='Password'></input>
+        <Link to="/chat">
+          <button className='bg-gray-900 text-white rounded-md w-96 py-2 '>Sign Up</button>
+        </Link>
+      </div>
+      <p className="text-gray-600">Already have an account? <Link to="/login"><span className="text-gray-900">Log in</span></Link> </p>
+    </div>
   )
 }
 
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import logo from "./assets/iconoir--page.svg"
+import loginicon from "./assets/material-symbols-light--login.svg"
+import signupicon from "./assets/octicon--person-add-24.svg"
 
 function Welcome() {
   return (
-    <div>Welcome</div>
+    <div className='flex items-center justify-center flex-col h-screen gap-8'>
+      <div className='flex flex-col items-center'>
+        <img src={logo} className='h-8 mb-5'></img>
+        <p className='text-gray-900 font-extrabold text-2xl'>Welcome to VerbiSense App</p>
+        <p className='text-gray-600'>Your AI-powered question answering assistant</p>
+      </div>
+      <div>
+        <Link to="/login">
+        <button className='bg-gray-900 text-white flex gap-3 justify-center w-96 py-2 mb-5 rounded-md'>
+          <img src={loginicon} className='h-6'></img>
+          Log In
+        </button>
+        </Link>
+        <Link to="/signup">
+        <button className='flex gap-3 justify-center w-96 py-2 border-2 border-gray-300 rounded-md'>
+          <img src={signupicon} className='h-6'></img>
+          Sign Up
+        </button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
