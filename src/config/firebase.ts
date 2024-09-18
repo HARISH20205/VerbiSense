d2b5be5a52310d89f67956ba09be9502e18fcdb5
Daranivelan <daranivelan10@gmail.com>
2024-09-18 13:25:58 +0530

    Firebase Setup

---
+++ src/config/firebase.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.API_KEY,
+  authDomain: "verbisense.firebaseapp.com",
+  projectId: "verbisense",
+  storageBucket: "verbisense.appspot.com",
+  messagingSenderId: "406831934655",
+  appId: "1:406831934655:web:107adc2a6e35a9f9f02a0a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
